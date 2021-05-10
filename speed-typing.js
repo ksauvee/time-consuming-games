2d79aa7b1720c94a3155244d316d23195a6ec768
kylian <kylian@sauvee.com>
2021-05-10 17:32:21 +0200

    refactor speed-typing.js

--- speed-typing.js
+++ speed-typing.js
@@ -1,9 +1,5 @@
 const chosen_text = document.getElementById('chosen-text')
 
-function fill_chosen_text(text) {
-    chosen_text.textContent = text
-}
-
 
 function get_random_int(max) {
     return Math.floor(Math.random() * max)
@@ -11,14 +7,13 @@ function get_random_int(max) {
 
 
 function get_text() {
-    text = ""
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(function (data) {
         random_int = get_random_int(data.length)
-        text = data[random_int].title
-        fill_chosen_text(text)
+        chosen_text.textContent = data[random_int].title
     })
 }
+
 
 get_text()
