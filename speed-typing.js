8b0203c9f844acb26307eb59d489a4a90569f99d
kylian <kylian@sauvee.com>
2021-05-10 17:16:16 +0200

    add chosen-text

---
+++ speed-typing.js
@@ -0,0 +1,21 @@
+const chosen_text = document.getElementById('chosen-text')
+
+function fill_chosen_text(text) {
+    chosen_text.textContent = text
+}
+
+
+fill_chosen_text("Let me not to the marriage of true minds\n" +
+    "Admit impediments. Love is not love\n" +
+    "Which alters when it alteration finds,\n" +
+    "Or bends with the remover to remove.\n" +
+    "O no, it is an ever-fixed mark\n" +
+    "That looks on tempests and is never shaken;\n" +
+    "It is the star to every wand'ring barque,\n" +
+    "Whose worth's unknown, although his height be taken.\n" +
+    "Love's not Time's fool, though rosy lips and cheeks\n" +
+    "Within his bending sickle's compass come;\n" +
+    "Love alters not with his brief hours and weeks,\n" +
+    "But bears it out even to the edge of doom.\n" +
+    "If this be error and upon me proved,\n" +
+    "I never write, nor no man ever loved.")
