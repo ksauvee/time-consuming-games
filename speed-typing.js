c780548e0cccfb81ceb817c808220f199365b308
kylian <kylian@sauvee.com>
2021-05-10 17:27:57 +0200

    add random text

--- speed-typing.js
+++ speed-typing.js
@@ -5,17 +5,20 @@ function fill_chosen_text(text) {
 }
 
 
-fill_chosen_text("Let me not to the marriage of true minds\n" +
-    "Admit impediments. Love is not love\n" +
-    "Which alters when it alteration finds,\n" +
-    "Or bends with the remover to remove.\n" +
-    "O no, it is an ever-fixed mark\n" +
-    "That looks on tempests and is never shaken;\n" +
-    "It is the star to every wand'ring barque,\n" +
-    "Whose worth's unknown, although his height be taken.\n" +
-    "Love's not Time's fool, though rosy lips and cheeks\n" +
-    "Within his bending sickle's compass come;\n" +
-    "Love alters not with his brief hours and weeks,\n" +
-    "But bears it out even to the edge of doom.\n" +
-    "If this be error and upon me proved,\n" +
-    "I never write, nor no man ever loved.")
+function get_random_int(max) {
+    return Math.floor(Math.random() * max)
+}
+
+
+function get_text() {
+    text = ""
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(function (data) {
+        random_int = get_random_int(data.length)
+        text = data[random_int].title
+        fill_chosen_text(text)
+    })
+}
+
+get_text()
