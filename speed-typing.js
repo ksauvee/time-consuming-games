d7a0cb76f4da2132daa487df5bce54f6a376489a
kylian <kylian@sauvee.com>
2021-05-10 23:24:01 +0200

    add alert when typed text is correct

--- speed-typing.js
+++ speed-typing.js
@@ -1,4 +1,17 @@
 const chosen_text = document.getElementById('chosen-text')
+const submit_button = document.getElementById('submit-button')
+let start = Date.now()
+
+
+function is_typed_text_correct() {
+    let typed_text = document.getElementById('typed-text').value
+    const answer_time = Date.now() - start
+    if (typed_text === chosen_text.textContent) {
+        alert(Math.floor(answer_time / 1000) + ' seconds')
+        window.location.reload()
+        start = Date.now()
+    }
+}
 
 
 function get_random_int(max) {
@@ -6,14 +19,16 @@ function get_random_int(max) {
 }
 
 
-function get_text() {
+function get_chosen_text() {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
     .then(function (data) {
-        random_int = get_random_int(data.length)
+        const random_int = get_random_int(data.length)
         chosen_text.textContent = data[random_int].title
     })
 }
 
 
-get_text()
+document.getElementById('typed-text').value = ''
+get_chosen_text()
+submit_button.onclick = is_typed_text_correct
