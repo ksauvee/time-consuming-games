b7a79d16fc0750336b51857d06f9e998093aea0c
kylian <kylian@sauvee.com>
2021-06-01 22:35:39 +0200

    add buttons retry

--- games/speed-typing/speed-typing.js
+++ games/speed-typing/speed-typing.js
@@ -1,8 +1,8 @@
-function is_typed_text_correct(chosen_text, start, results) {
+function is_typed_text_correct(chosen_text, start, results, retry, retry_same_text) {
     let typed_text = document.getElementById('typed-text').value
     const answer_time = Date.now() - start
     if (typed_text === chosen_text.textContent) {
-        ending_message(results, answer_time)
+        ending_message(results, answer_time, retry, retry_same_text, start)
         return Date.now()
     }
 }
@@ -23,10 +23,30 @@ function get_chosen_text(chosen_text) {
 }
 
 
-function ending_message(results, answer_time) {
+function ending_message(results, answer_time, retry, retry_same_text, start) {
     results.innerHTML = "You type the text in " + Math.floor(answer_time / 1000) + " seconds !"
     results.style.backgroundColor = "#425664"
     results.style.color = "white"
+    results.style.display = "block"
+    retry.innerHTML = "Retry"
+    retry.style.backgroundColor = "#425664"
+    retry.style.color = "white"
+    retry.style.display = "block"
+    retry.onclick = function() {window.location.reload()}
+    retry_same_text.innerHTML = "Retry With Same Text"
+    retry_same_text.style.backgroundColor =  "#425664"
+    retry_same_text.style.color = "white"
+    retry_same_text.style.display = "block"
+    retry_same_text.onclick = function() {start = launch_retry(results, retry, retry_same_text)}
+}
+
+
+function launch_retry(results, retry, retry_same_text) {
+    results.style.display = "none"
+    retry.style.display = "none"
+    retry_same_text.style.display = "none"
+    document.getElementById('typed-text').value = null
+    return Date.now()
 }
 
 
@@ -34,10 +54,12 @@ function main() {
     const chosen_text = document.getElementById('chosen-text')
     const submit_button = document.getElementById('submit-button')
     const results = document.getElementById('results')
+    const retry = document.getElementById('retry')
+    const retry_same_text = document.getElementById('retry-same-text')
     let start = Date.now()
     document.getElementById('typed-text').value = ''
     get_chosen_text(chosen_text)
-    submit_button.onclick = function() {start = is_typed_text_correct(chosen_text, start, results)}
+    submit_button.onclick = function() {start = is_typed_text_correct(chosen_text, start, results, retry, retry_same_text)}
 }
 
 
