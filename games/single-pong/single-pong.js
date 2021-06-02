a89c72a6a213a51b6750bb8eb7f6e2c2e144356f
kylian <kylian@sauvee.com>
2021-06-02 18:11:23 +0200

    add ball

--- games/single-pong/single-pong.js
+++ games/single-pong/single-pong.js
@@ -2,20 +2,37 @@ function Paddle() {
     this.x = 10
     this.y = 30
 
-    this.draw = function(board) {
-        const ctx = board.getContext("2d")
-        console.log(this.y)
-        ctx.beginPath()
-        ctx.clearRect(0, 0, 1400, 600)
-        ctx.fillStyle = "#006e81"
+    this.draw = function(board, ctx) {
         ctx.fillRect(this.x, this.y, 3, 20)
     }
+}
+
+
+function Ball() {
+    this.x = 50
+    this.y = 50
+
+    this.draw = function(board, ctx) {
+        ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+}
+
+
+function draw(player, ball, board) {
+    const ctx = board.getContext("2d")
+    ctx.fillStyle = "#006e81"
+    ctx.beginPath()
+    ctx.clearRect(0, 0, board.width, board.height)
+    player.draw(board, ctx)
+    ball.draw(board, ctx)
 }
 
 
 function main() {
     const board = document.getElementById("board")
     const player = new Paddle()
+    const ball = new Ball()
 
     document.addEventListener("keydown", (e) => {
         if (e.key === 'z') {
@@ -25,7 +42,9 @@ function main() {
         }
     })
 
-    setInterval(function () {player.draw(board)}, 0.001)
+    setInterval(function () {
+        draw(player, ball, board)
+    }, 0.001)
 }
 
 
