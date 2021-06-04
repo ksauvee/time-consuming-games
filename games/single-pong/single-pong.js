057738d563b429d1ebc27797d21871a331d8b656
kylian <kylian@sauvee.com>
2021-06-04 10:49:34 +0200

    clean single-pong

--- games/single-pong/single-pong.js
+++ games/single-pong/single-pong.js
@@ -2,6 +2,14 @@ function Paddle() {
     this.x = 10
     this.y = 30
 
+    this.getY = function () {
+        return this.y
+    }
+
+    this.setY = function(y) {
+        this.y = y
+    }
+
     this.draw = function(board, ctx) {
         ctx.fillRect(this.x, this.y, 3, 30)
     }
@@ -12,24 +20,28 @@ function Ball() {
     this.x = 50
     this.y = 50
     this.velX = 2
-    this.velY = 2
-    this.points = 0
+    this.velY = 0.5
 
-    this.draw = function (board, player, ctx) {
-        this.move(board, player)
+    this.draw = function (board, player, ctx, points) {
+        points = this.move(board, player, points)
         ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI)
         ctx.fill()
+        return points
     }
 
-    this.move = function(board, player) {
+    this.move = function(board, player, points) {
         if (this.x + 5 >= board.width) {
+            // case hit right wall
             this.velX = -this.velX
-        } else if (this.x - 5 <= player.x && this.y >= player.y && this.y <= player.y + 30) {
+        } else if (this.x - 5 <= player.x && this.x - 10 > 0 && this.y >= player.y && this.y <= player.y + 30) {
+            // case hit paddle
             this.velX = -this.velX
-            this.points += 1
+            points += 1
         } else if (this.y + 5 >= board.height || this.y - 5 <= 0) {
+            // case hit horizontal walls
             this.velY = -this.velY
-        } else if (this.x <= 0) {
+        } else if (this.x - 5 <= 0) {
+            // case hit left wall
             this.velX = 0
             this.velY = 0
             this.x = -5
@@ -37,21 +49,30 @@ function Ball() {
             clearInterval()
             const results = document.getElementById('results')
             const retry = document.getElementById('retry')
-            endingMessage(results, retry, this.points)
+            endingMessage(results, retry, points)
         }
         this.x += this.velX
         this.y += this.velY
+        return points
     }
 }
 
 
-function draw(player, ball, board) {
+function draw(player, ball, board, points) {
     const ctx = board.getContext("2d")
     ctx.fillStyle = "#006e81"
     ctx.beginPath()
     ctx.clearRect(0, 0, board.width, board.height)
     player.draw(board, ctx)
-    ball.draw(board, player, ctx)
+    points = ball.draw(board, player, ctx, points)
+    return points
+}
+
+
+function stylizeEndingMessage(element) {
+    element.style.backgroundColor = "#425664"
+    element.style.color = "white"
+    element.style.display = "block"
 }
 
 
@@ -59,13 +80,9 @@ function endingMessage(results, retry, points) {
     document.getElementsByTagName("header")[0].style.opacity = "0.2"
     document.getElementsByTagName("canvas")[0].style.opacity = "0.2"
     results.innerHTML = "You got " + points + " points !"
-    results.style.backgroundColor = "#425664"
-    results.style.color = "white"
-    results.style.display = "block"
+    stylizeEndingMessage(results)
     retry.innerHTML = "Retry"
-    retry.style.backgroundColor = "#425664"
-    retry.style.color = "white"
-    retry.style.display = "block"
+    stylizeEndingMessage(retry)
     retry.onclick = function() {window.location.reload()}
 }
 
@@ -74,21 +91,22 @@ function main() {
     const board = document.getElementById("board")
     const player = new Paddle()
     const ball = new Ball()
+    let points = 0
 
     document.addEventListener("keydown", (e) => {
         if (e.key === 'z') {
-            if (player.y < board.height - 30) {
-                player.y += 15
+            if (player.getY() < board.height - 30) {
+                player.setY(player.getY() + 15)
             }
         } else if (e.key === 's') {
-            if (player.y > 0) {
-                player.y -= 15
+            if (player.getY() > 0) {
+                player.setY(player.getY() - 15)
             }
         }
     })
 
     setInterval(function () {
-        draw(player, ball, board)
+        points = draw(player, ball, board, points)
     }, 1000/60)
 }
 
